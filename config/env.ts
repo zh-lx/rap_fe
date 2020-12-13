f2754a86c4a044f6a7df4b526fbf5425b438dc38
zh-lx <18366276315@163.com>
2020-12-13 21:40:46 +0800

    feat: 根据env判断api的host

--- config/env.ts
+++ config/env.ts
@@ -1,1 +1,4 @@
-export const apiPath = 'http://server.rap.zlxiang.com';
+export const apiPath =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8801'
+    : 'http://server.rap.zlxiang.com';
